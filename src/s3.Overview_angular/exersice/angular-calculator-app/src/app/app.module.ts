05a71b505fe349467d28063bd72452c96cd7823d
Your Namenguyenvandong0798 <you@example.comnguyenvandong0798@gmail.com>
2022-01-26 07:35:05 +0700

    commit

--- src/s3.Overview_angular/exersice/angular-calculator-app/src/app/app.module.ts
+++ src/s3.Overview_angular/exersice/angular-calculator-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CalculatorComponent } from './calculator/calculator.component';
     CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/s3.Overview_angular/exersice/angular-calculator-app/src/app/calculator/calculator.component.ts
+++ src/s3.Overview_angular/exersice/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  public numberOne = 0 ;
+  public numberTwo = 0 ;
+  public result = 0 ;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  congNum():void{
+    this.result = Number(this.numberOne) + Number(this.numberTwo);
+  }
+  truNum():void{
+    this.result = Number(this.numberOne) - Number(this.numberTwo);
+  }
+  nhanNum():void{
+    this.result = Number(this.numberOne) * Number(this.numberTwo);
+  }
+  chiaNum():void{
+    this.result = Number(this.numberOne) / Number(this.numberTwo);
   }
 
 }
